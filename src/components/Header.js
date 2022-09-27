c5b34dde2d9cb8b8ebd6e88e0cceb1e054153407
AldoStevhen <34674358+AldoStevhen@users.noreply.github.com>
2022-09-27 20:33:50 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 // atau langsung tambahkan dengan sx={{}}
-import React from 'react';
+import { Divider, Typography } from "@mui/material";
+import React from "react";
 
 // Header berisi judul dan subtitle
 // Judul: Call a Friend
 // Subtitle: Your friendly contact app
 const Header = () => {
   return (
-    <></>
+    <>
+      <Typography variant="h1" component="h2">
+        call a friend
+      </Typography>
+      <Divider>your friendly contact app</Divider>
+    </>
   );
 };
 
