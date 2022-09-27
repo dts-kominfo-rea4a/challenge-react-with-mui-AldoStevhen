9f171889286c1f5f7742b655dd482338c414f056
AldoStevhen <34674358+AldoStevhen@users.noreply.github.com>
2022-09-27 20:31:27 +0800

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,48 @@
 // Terima props lalu tampilkan dalam Contact component
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
-import React from 'react';
+import { Avatar, Box, Divider, List, ListItem, Typography } from "@mui/material";
+import React, { Fragment } from "react";
+
+const style = {
+  width: "100%",
+  maxWidth: 600,
+  bgcolor: "#DBF6F0",
+  padding: "10px",
+};
+
+const DetailContact = (props) => {
+  const { name, phone, email } = props;
+
+  return (
+    <>
+      <Box sx={{ marginLeft: "20px" }}>
+        <Typography sx={{ fontWeight: "bold" }}>{name}</Typography>
+        <Typography>{phone}</Typography>
+        <Typography>{email}</Typography>
+      </Box>
+    </>
+  );
+};
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
-    // Contact berisi foto, nama, telepon, dan email
-    return (<></>);
+  // Contact berisi foto, nama, telepon, dan email
+
+  return (
+    <>
+      <List sx={style} component="nav" aria-label="mailbox folders">
+        <Fragment>
+          <ListItem>
+            <Avatar src={data.photo} alt={data.photo} sx={{ width: 80, height: 80 }} />
+            <DetailContact name={data.name} email={data.email} phone={data.phone} />
+          </ListItem>
+          <Divider light />
+        </Fragment>
+      </List>
+    </>
+  );
 };
 
 export default Contact;
